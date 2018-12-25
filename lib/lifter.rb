c90302bd6b1d0ff40753b2471cba896ce106fb05
github username <github email address>
2018-12-25 16:18:12 -0500

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,36 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select { |membership| membership.lifter == self }
+  end
+
+  def gyms
+    memberships.map { |membership| membership.gym }
+  end
+
+  def self.avg_lift
+    self.all.inject(0) { |total, lifter| total + lifter.lift_total }.to_f / self.all.count
+  end
+
+  def total_cost
+    self.memberships.inject(0) { |total, member| total + member.cost }
+  end
+
+  def new_gym(cost, gym)
+    Membership.new(cost, self, gym)
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,19 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+kah = Lifter.new("Kah", 185)
+ludy = Lifter.new("Ludy", 45)
+
+i = Gym.new("Iroh's Gym")
+b = Gym.new("Blink")
+r = Gym.new("Rec")
+
+ki = Membership.new(0, kah, i)
+kb = Membership.new(27, kah, b)
+kr = Membership.new(25, kah, r)
+
+ludy.new_gym(20, i)
+
 
 binding.pry
 
